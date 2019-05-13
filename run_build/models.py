62fddeed5b3f97aeb5370f170cff893aa3477391
domoramas <boombappro@gmail.com>
2019-05-13 14:15:09 -0700

    added templates folder for html

--- run_build/models.py
+++ run_build/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class BlogEntry(models.Model):
+  blog_title = models.CharField(max_length = 50)
+  blog_text = models.CharField(max_length = 2000)
+  pub_date = models.DateTimeField('date published')
