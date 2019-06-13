9b811f7371a6a021969ea2a28368f3872363b9ff
domoramas <boombappro@gmail.com>
2019-06-13 10:11:30 -0700

    working on templates

--- rbs_site/urls.py
+++ rbs_site/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-
+app_name = "rbs_site"
 urlpatterns = [
-  path( '', views.index, name = 'index')
+  path( '', views.HomepageView.as_view(), name = 'home')
 ]
--- rbs_site/views.py
+++ rbs_site/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Event
 
 # Create your views here.
 
 
-def index(request):
-  return HttpResponse("hello  there")
+class HomepageView(generic.ListView):
+  model = Event
+  template_name = "rbs_site/main.html"
